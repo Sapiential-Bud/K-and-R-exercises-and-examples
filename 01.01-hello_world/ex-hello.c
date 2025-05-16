a99e434e787656e2a5f982966f9ca19e1e94e88b
dongyumengya <2394666017@qq.com>
2025-05-17 06:27:36 +0800

    Add CMakeLists

--- 01.01-hello_world/ex-hello.c
+++ 01.01-hello_world/ex-hello.c
@@ -5,5 +5,11 @@ int main(void)
 {
   printf("hello, world \n");
 
+
+  int x = 42;
+  printf("Address of x: %p\n", (void*)&x);  // from stack
+
+  printf("Address of string: %p\n", (void*)"hello");
+
   return 0;
 }
